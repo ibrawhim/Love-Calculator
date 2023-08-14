0e492935d2f894109a9ebd6d817d1d4ca6d0be83
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-08-14 02:15:09 +0100

    mini

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Back from '../images/Back.jpg'
+
+let myLink = {
+  width: '100vw',
+  height: '100vh',
+  backgroundImage: 'url(/src/images/Back.jpg)',
+  backgroundSize: '100vw 100vh'
+}
 const Home = () => {
   return (
     <>
-     <Link to="/addbudget">Go to add budget</Link> 
+    <div style={myLink}>
+     <Link className='d-flex justify-content-center align-items-center top-50' to="/addbudget">Go to add budget</Link> 
+    </div>
+
     </>
   )
 }
